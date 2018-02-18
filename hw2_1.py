56f6ea678453c9bfa2968633ef13f73f0f1eb956
Chawasit Tengtrairatana <chawasit555@gmail.com>
2018-02-18 17:12:25 +0700

    refactored calculation part

--- hw2_1.py
+++ hw2_1.py
@@ -1,7 +1,4 @@
-def main():
-    possible_outcome = int(raw_input("Number of possible outcome: "))
-    number_of_things_to_be_chosen = int(raw_input("Number of things to be chosen: "))
-
+def calculate_probability_of_no_coincident(number_of_things_to_be_chosen, possible_outcome):
     probability_of_no_coincident = 1.0
 
     if number_of_things_to_be_chosen > 1:
@@ -9,8 +6,19 @@ def main():
             probability_of_no_coincident = \
                 (1.0 - ((i - 1) / float(possible_outcome))) * probability_of_no_coincident
     
+    return probability_of_no_coincident
+
+
+def main():
+    possible_outcome = int(raw_input("Number of possible outcome: "))
+    number_of_things_to_be_chosen = int(raw_input("Number of things to be chosen arbitrarily: "))
+
+    probability_of_no_coincident = \
+        calculate_probability_of_no_coincident(number_of_things_to_be_chosen, possible_outcome)
+    
     output_format = "The probability of n chosen things are difference is {}"
     print output_format.format(probability_of_no_coincident)
+
 
 if __name__ == '__main__':
     main()
