e219d0945886de27880152b13094aff07f710f2f
Chawasit Tengtrairatana <chawasit555@gmail.com>
2018-02-18 17:08:17 +0700

    fixed typo

--- hw2_1.py
+++ hw2_1.py
@@ -1,15 +1,15 @@
 def main():
     possible_outcome = int(raw_input("Number of possible outcome: "))
-    number_of_event = int(raw_input("Number of choosen people: "))
+    number_of_things_to_be_chosen = int(raw_input("Number of things to be chosen: "))
 
     probability_of_no_coincident = 1.0
 
-    if number_of_event > 1:
-        for i in range(2, number_of_event + 1):
+    if number_of_things_to_be_chosen > 1:
+        for i in range(2, number_of_things_to_be_chosen + 1):
             probability_of_no_coincident = \
                 (1.0 - ((i - 1) / float(possible_outcome))) * probability_of_no_coincident
     
-    output_format = "The probability of n choosen thing are difference = {}"
+    output_format = "The probability of n chosen things are difference is {}"
     print output_format.format(probability_of_no_coincident)
 
 if __name__ == '__main__':
